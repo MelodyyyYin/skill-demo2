65233ca4a5b08c1bd46ef15c851b0055380e5df7
MelodyyyYin <melodyyin31@gmail.com>
2022-02-03 11:49:15 -0800

    lib + fail test

--- skilldemo.java
+++ skilldemo.java
@@ -1,1 +1,5 @@
-public class skilldemo{}
+public class skilldemo{
+    public static int multiple(int n, int m){
+        return n * m;
+    }
+}
---
+++ skilldemoTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skilldemoTest {
+    @Test
+    public void addition() {
+        assertEquals(2, skilldemo.multiple(3, 5));//this should fail
+    }
+}
+
