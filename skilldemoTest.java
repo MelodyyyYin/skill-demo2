13c9947a14e83837ffd986c6a2c5e89100823016
MelodyyyYin <melodyyin31@gmail.com>
2022-02-03 11:53:08 -0800

    right one

--- skilldemoTest.java
+++ skilldemoTest.java
@@ -4,7 +4,7 @@ import org.junit.*;
 public class skilldemoTest {
     @Test
     public void addition() {
-        assertEquals(2, skilldemo.multiple(3, 5));//this should fail
+        assertEquals(15, skilldemo.multiple(3, 5));
     }
 }
 
